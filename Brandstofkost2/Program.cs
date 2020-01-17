9719b7e80349ae34f232b4eec3c4064e1c8968c5
Enrick De Munter <enrick.demunter@student.romerocollege.be>
2020-01-17 11:57:02 +0100

    Add project files.

---
+++ Brandstofkost2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Enrick De Munter nr3 5ITN 17/01/2020
+/*
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brandstofkost2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
